01a9b675efb7571ff1c8a3f20de3387cc30ae9e5
Tamara <tlumumba@gmail.com>
2023-09-22 01:58:57 -0700

    A function that sorts using bubble algo

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+#include "stdio.h"
+
+/**
+ * bubble_sort - a function that sorts an array of integers in
+                 ascending order using the Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: The number of elements in an array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i,j,temp;
+
+    for(i = 0; i < size; i++)
+    {
+        for(j = 0; j < size - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array,size);
+
+            }
+        }
+    }
+}
